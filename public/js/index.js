07033398b809ff67ad1a09a71bc7a3620d1a2187
Jatinder Pal Singh <jatinderpalsingh@Jatinders-MacBook-Pro.local>
2018-02-10 02:42:09 +0530

    scroll to bottom to keep latest messages in view area

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+    //selectors
+    var messages = $("#messages");
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('connected to server');
 });
@@ -17,6 +33,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     $("#messages").append(html);
+    scrollToBottom();
     // var chatContainer = $("#messages");
     // var newMessage = $('<li></li>');
     // var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -34,6 +51,7 @@ socket.on('newLocationMessage', function(message) {
         createdAt: formattedTime,
     });
     $("#messages").append(html);
+    scrollToBottom();
     // console.log('got location message ', message);
     // var newLocationLi = $('<li></li>');
     // var formattedTime = moment(message.createdAt).format('h:mm a');
