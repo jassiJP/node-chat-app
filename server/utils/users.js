38a05304ad2eefe5b09fd1740d3f2f9e0c30ae3c
Jatinder Pal Singh <jatinderpalsingh@Jatinders-MacBook-Pro.local>
2018-02-10 04:32:03 +0530

    test

--- server/utils/users.js
+++ server/utils/users.js
@@ -23,6 +23,25 @@ class Users {
         this.users.push(user);
         return user;
     }
+
+    removeUser (id) {
+        //return user that was removed
+        var user = this.getUser(id);
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+        return user;
+    }
+
+    getUser (id) {
+        return this.users.filter((user) => user.id === id)[0];
+    }
+
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+        return namesArray;
+    }
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,26 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+
+    var users;
+
+    beforeEach(() => {
+        users = new Users();
+        users.users = [{
+            id: '123',
+            name: 'Jatinder',
+            room: 'home'
+        }, {
+            id: '124',
+            name: 'Ginni',
+            room: 'office'
+        }, {
+            id: '125',
+            name: 'Mummy',
+            room: 'home'
+        }]
+    });
+
     it('should add new user', () => {
         var users = new Users();
 
@@ -14,5 +34,43 @@ describe('Users', () => {
         var resUser = users.addUser(user.id, user.name, user.room);
 
         expect(users.users).toMatchObject([user]);
+    });
+
+    it('should remove the user', () => {
+        var userId = '123';
+        var user = users.removeUser(userId);
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should not remove a user', () => {
+        var userId = '111';
+        var user = users.removeUser(userId);
+        expect(user).toBeFalsy();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('should find user', () => {
+        var userId = '123';
+        var user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    });
+
+    it('should not find a user', () => {
+        var userId = '111';
+        var user = users.getUser(userId);
+
+        expect(user).toBeFalsy();
+    });
+
+    it('should return name for home room', () => {
+        var userList = users.getUserList('home');
+        expect(userList).toEqual(['Jatinder', 'Mummy']);
+    });
+
+    it('should return name for office room', () => {
+        var userList = users.getUserList('office');
+        expect(userList).toEqual(['Ginni']);
     });
 });
